86e1c083dccb4f338c9102c0bd7d685ef4f10604
nakadaole <genesis@nakadaole.com>
2021-06-29 23:38:31 +0800

    can monitoring liquidity event

--- src/Ape.ts
+++ src/Ape.ts
@@ -1,34 +1,33 @@
 import Web3 from 'web3'
-
+import {fromWei} from 'web3-utils'
+import Utils from './Utils'
+import {Topics,Symbols,Reserve } from './Models'
+import { Console } from 'console';
 
 export default class Ape{
 
     // web3 provider
     private web3: Web3;
-    private routerAddress: string = process.env["ROUTER_ADDRESS"];
+    private routerAddress: string = process.env.ROUTER_ADDRESS;
     private factoryAddress: string = process.env.FACTORY_ADDRESS;
     private defaultGas = process.env.GAS_PRICE;
-
-
-    // pair created event
-    private Topics = {
-        PairCreated: '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9',
-    };
-    
+    private abiDecoder = require('abi-decoder');
 
     public constructor() {
         this.web3 = new Web3(process.env.WEB3_WS_PROVIDER);
+
+        // load ABIs into decoder
+        this.abiDecoder.addABI(require('../ABIs/IPancakeFactoryV2.json'))
+        this.abiDecoder.addABI(require('../ABIs/IPancakeRouterV2.json'))
+
         this.watch();
     }
 
-    private router(){
-        return new this.web3.eth.Contract(require('../ABIs/IPancakeRouterV2.json'), this.routerAddress);
-    }
-
+    // Start monitoring pair created events
     public async watch(){
         this.web3.eth.subscribe('logs',{
             address: this.factoryAddress,
-            topics: [this.Topics.PairCreated],
+            topics: [Topics.PairCreated],
         })
         .on('data',(log)=>{
             this.handleLogs(log);
@@ -42,7 +41,45 @@ export default class Ape{
         })
     }
 
-    private async handleLogs(logs: any){
-        console.log(logs);
+    private async handleLogs(log :any){
+        const decodedData = this.abiDecoder.decodeLogs([log]);
+        const values = Utils.decodedEventsToArray(decodedData[0]);
+
+        if(values.token0 !== Symbols.wbnb && values.token1 !== Symbols.wbnb){
+            return;
+        }
+
+        const reserve = await this.getReserve(values.pair);
+
+        const bnbReserve = values.token0 === Symbols.wbnb ? reserve.reserve0 : reserve.reserve1;
+
+        if(bnbReserve.eq(0)){ return; }
+
+        console.log(`New pair created: ${values.pair} BNB reserve: ${fromWei(bnbReserve.toFixed())}`);
+
     }
+
+    private getReserve(pair :string){
+        return new Promise<Reserve>((resolve, reject)=>{
+            const PairContract = new this.web3.eth.Contract(require('../ABIs/IPancakePair.json'), pair);
+            PairContract.methods.getReserves().call()
+                .then((result: any)=>{
+                    resolve(new Reserve(result[0], result[1]));
+                })
+                .catch((error:any)=>{
+                    reject(error);
+                })
+        });
+    }
+
+    // Pancake Router Contract Instantce
+    private router(){
+        return new this.web3.eth.Contract(require('../ABIs/IPancakeRouterV2.json'), this.routerAddress);
+    }
+
+    // pancake Facotry Contract Instance
+    private factory(){
+        return new this.web3.eth.Contract(require('../ABIs/IPancakeFactoryV2.json'), this.routerAddress);
+    }
+    
 }
---
+++ src/Models.ts
@@ -0,0 +1,22 @@
+import BigNumber from "bignumber.js";
+
+export class Reserve
+{
+    public reserve0: BigNumber;
+    public reserve1: BigNumber;
+
+    constructor(r0: string, r1: string) {
+        this.reserve0 = new BigNumber(r0);
+        this.reserve1 = new BigNumber(r1);
+    }
+}
+
+export const Symbols = {
+    wbnb: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', // mainnet
+    //wbnb: '0xae13d989dac2f0debff460ac112a837c89baa7cd', // testnet
+}
+
+// pair created event
+export const Topics = {
+    PairCreated: '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9',
+};
